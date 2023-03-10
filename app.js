2642e304403b6e76cdd5e75aceb0dd47aed7e2dc
merve kartal <mervekartal@merves-MacBook-Air.local>
2023-03-10 15:15:54 +0300

    partials pages added

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.use(express.static('public'))
 app.get('/', (req,res) => {
     res.render('index')
 })
+app.get('/about', (req,res) => {
+    res.render('about')
+})
 
 app.listen(port, () => {
     console.log(`App running on port: ${port}`)
